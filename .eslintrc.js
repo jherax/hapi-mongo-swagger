171f012be4fd6b94a6348e8d0726f2d997725a2f
David Rivera <jherax@gmail.com>
2023-07-25 17:09:01 -0500

    🚀 Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+/**
+ * @see https://dev.to/brayanarrieta/how-to-integrate-eslint-with-your-react-typescript-project-2021-182n
+ */
+module.exports = {
+  plugins: ['react', 'simple-import-sort', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier', // this should be the last
+  ],
+  root: true, // Make sure eslint picks up the config at the root of the directory
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021, // Use the latest ecmascript standard
+    sourceType: 'module', // Allows using import/export statements
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es2021: true,
+    browser: true, // Enables browser globals like window and document
+    node: true, // Enables Node.js global variables and Node.js scoping.
+    jest: true,
+  },
+  globals: {},
+  rules: {
+    curly: 'error',
+    'no-console': ['error', {allow: ['warn', 'error', 'info']}],
+    'spaced-comment': ['warn', 'always'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# hapi-mongo-swagger
+
+[GraphQL](https://graphql.org/) API based on Nodejs with
+[Swagger](https://swagger.io/) documentation powered by
+[MongoDB](https://www.mongodb.com/)
