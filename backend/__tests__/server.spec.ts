27027b8ef498ea82252503e0229c768835422997
David Rivera <jherax@gmail.com>
2023-07-26 16:04:51 -0500

    build: Configured Jest and added unit tests

---
+++ backend/__tests__/server.spec.ts
@@ -0,0 +1,40 @@
+import type {Server} from '@hapi/hapi';
+
+import {init} from '../server';
+
+/**
+ * The init() function will initialize the server (starts cache,
+ * finalizes plugin registration) but does not start the server.
+ * This is what you will use in your tests. The start() function
+ * will actually start the server. This is what you will use in our
+ * main entry-point for the server.
+ *
+ * @see https://hapi.dev/tutorials/testing
+ * @see https://blog.logrocket.com/testing-typescript-apps-using-jest/
+ */
+
+describe('Server GET "/"', () => {
+  let server: Server;
+
+  beforeEach(async () => {
+    server = await init();
+  });
+
+  afterEach(async () => {
+    await server.stop();
+  });
+
+  it('responds with status code 200', async () => {
+    const reply = await server.inject({
+      method: 'get',
+      url: '/',
+    });
+
+    expect(reply.statusCode).toBe(200);
+    expect(reply.result).toBe(getExpectedText());
+  });
+});
+
+function getExpectedText() {
+  return `<h1>GraphQL API with Nodejs, Swagger and MongoDB</h1>`;
+}
